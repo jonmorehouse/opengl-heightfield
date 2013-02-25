937b36641601e3e0be88780f980baebf45eca20c
Jon Morehouse <morehousej09@gmail.com>
2013-02-25 01:50:14 -0800

    Updated gitignore and built compile script

--- compile.sh
+++ compile.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
 # this is a short bash script to programmatically create an animation from screenshots using imagemagick
+interval=5
 
-interval=${1}
-folder=${2}
-output=${3}
-
-if [[ ${#1} -eq 0 || ${#2} -eq 0 || ${#3} -eq 0 ]]; then
-	echo "Proper usage: compile interval folder output.mp4"
-	exit
-fi
+# create filler gap images
+convert screenshots/*.png -delay $interval -morph $interval temp/%05d.morph.png
 
 # q scale = quality where 1 = highest and 32 = lowest
-# ffmpeg -r 25 -qscale 2 -i %05d.morph.jpg output.mp4
+ffmpeg -r 25 -qscale 2 -i temp/%05d.morph.png output.mp4
